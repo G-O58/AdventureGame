ce6dce705e3d942d8535eb6511f7fafdf939b129
Greg <S-Gregory.Borrow@lwtech.edu>
2020-04-19 21:47:06 -0700

    nolonger recursive

--- AdventureGame.java
+++ AdventureGame.java
@@ -11,7 +11,10 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class AdventureGame {
-	
+	private static enum Page {
+		town, winGame, crossRoad, river, forest, goblin, fight, attack, winFight, loseGame
+	}
+
 	private static Scanner myScanner = new Scanner(System.in);
 	private static Scanner enterScanner = new Scanner(System.in);
 	public static Random myRandom = new Random();
@@ -24,12 +27,53 @@ public class AdventureGame {
 	private static boolean silverRing = false;
 		
 	public static void main(String[] args) {
-		initializePlayer(); 	
-		approachTown();
+		Page nextPage = Page.town;
+		Page currentPage;
+		boolean isPlaying = true;
+		initializePlayer(); 
+		do {
+			currentPage = nextPage;
+			switch (currentPage){
+				case town:
+					nextPage = approachTown();
+					break;
+				case winGame:
+					winGame();
+					isPlaying = false;
+					break;
+				case loseGame:
+					loseGame();
+					isPlaying = false;
+					break;
+				case crossRoad:
+					nextPage = approachCrossRoad();
+					break;
+				case river:
+					nextPage = approachRiver();
+					break;
+				case forest:
+					nextPage = approachForest();
+					break;
+				case goblin:
+					nextPage = approachGoblin();
+					break;
+				case fight:
+					nextPage = fight();
+					break;
+				case attack:
+					nextPage = attack();
+					break;
+				case winFight:
+					nextPage = winFight();
+					break;
+				default:
+					nextPage = Page.town;
+					break;
+			}
+		} while (isPlaying);
 	}
 	
 	private static void initializePlayer() {
-		
 		playerHP = 10;
 		monsterHP = 15;
 		playerWeapon = "Knife";
@@ -41,8 +85,9 @@ public class AdventureGame {
 		System.out.println("Hello " + playerName + ", let's start the game!");	
 	}	
 	
-	private static void approachTown() {
-		
+	private static Page approachTown() {
+		//TODO remove all text brackets
+		{ //text
 		System.out.println("\n------------------------------------------------------------------\n");
 		System.out.println("You are at the gate of the town.");
 		System.out.println("A guard is standing in front of you.");
@@ -53,34 +98,38 @@ public class AdventureGame {
 		System.out.println("2: Attack the guard");
 		System.out.println("3: Leave");
 		System.out.println("\n------------------------------------------------------------------\n");
+		}
 		choice = myScanner.nextInt();
 
+		Page chosenPage;
 		switch (choice) {
 			case 1:
-				if (silverRing) {winGame();} 
+				if (silverRing) { chosenPage = Page.winGame;} 
 				else {
 					System.out.println("Guard: Hello there, stranger. So your name is " + playerName + "? \nSorry but we cannot let stranger enter our town.");
 					enterScanner.nextLine();
-					approachTown();
+					chosenPage = Page.town;
 				}
 				break;
 			case 2:
-				playerHP = playerHP-1;
+				playerHP = playerHP - 1;
 				System.out.println("Guard: Hey don't be stupid.\n\nThe guard hit you so hard and you gave up.\n(You receive 1 damage)\n");
 				System.out.println("Your HP: " + playerHP);
 				enterScanner.nextLine();
-				approachTown();
+				chosenPage = Page.town;
 				break;
 			case 3:
-				crossRoad();
+				chosenPage = Page.crossRoad;
 				break;
 			default:
-				approachTown();
+				chosenPage = Page.town;
 				break;
 		}
+		return chosenPage;
 	}
 	
-	private static void crossRoad() {
+	private static Page approachCrossRoad() {
+		{ //text
 		System.out.println("\n------------------------------------------------------------------\n");
 		System.out.println("You are at a crossroad. If you go south, you will go back to the town.\n\n");
 		System.out.println("1: Go north");
@@ -88,123 +137,137 @@ public class AdventureGame {
 		System.out.println("3: Go south");
 		System.out.println("4: Go west");
 		System.out.println("\n------------------------------------------------------------------\n");
+		}
 		choice = myScanner.nextInt();
-		
+		Page chosenPage;
 		switch (choice) {
 			case 1:
-				approachRiver();
+				chosenPage = Page.river;
 				break;
 			case 2:
-				approachForest();
+				chosenPage = Page.forest;
 				break;
 			case 3:
-				approachTown();
+				chosenPage = Page.town;
 				break;
 			default:
-				approachGoblin();
+				chosenPage = Page.goblin;
 				break;
 		}
+		return chosenPage;
 	}
 	
-	private static void approachRiver() {
+	private static Page approachRiver() {
+		playerHP = playerHP + 1;
+		{ //text
 		System.out.println("\n------------------------------------------------------------------\n");
 		System.out.println("There is a river. You drink the water and rest at the riverside.");
 		System.out.println("Your HP is recovered.");
-		playerHP = playerHP + 1;
 		System.out.println("Your HP: " + playerHP);
 		System.out.println("\n\n1: Go back to the crossroad");
 		System.out.println("\n------------------------------------------------------------------\n");
+		}
 		choice = myScanner.nextInt();
 		
 		if (choice == 1) {
-			crossRoad();
+			return Page.crossRoad;
 		}
 		else {
-			approachRiver();
+			return Page.river;
 		}
 	}
 	
-	private static void approachForest() {
+	private static Page approachForest() {
+		playerWeapon = "Long Sword";
+		{ //text
 		System.out.println("\n------------------------------------------------------------------\n");
 		System.out.println("You walked into a forest and found a Long Sword!");
-		playerWeapon = "Long Sword";
 		System.out.println("Your Weapon: "+ playerWeapon);
 		System.out.println("\n\n1: Go back to the crossroad");
 		System.out.println("\n------------------------------------------------------------------\n");
+		}
 		choice = myScanner.nextInt();
 		
 		if (choice == 1) {
-			crossRoad();
+			return Page.crossRoad;
 		}
 		else {
-			approachForest();
+			return Page.forest;
 		}
 	}
 	
-	private static void approachGoblin() {
+	private static Page approachGoblin() {
+		{//text
 		System.out.println("\n------------------------------------------------------------------\n");
 		System.out.println("You encounter a goblin!\n");
 		System.out.println("1: Fight");
 		System.out.println("2: Run");
 		System.out.println("\n------------------------------------------------------------------\n");
+		}
 		choice = myScanner.nextInt();
-		
+		Page chosenPage;
 		switch (choice) {
 			case 1:
-				fight();
+				chosenPage = Page.fight;
 				break;
 			case 2:
-				crossRoad();
+				chosenPage = Page.crossRoad;
 				break;
 			default:
-				approachGoblin();
+				chosenPage = Page.goblin;
+				break;
 		}
+		return chosenPage;
 	}
 	
-	private static void fight() {
+	private static Page fight() {
+		{ //text
 		System.out.println("\n------------------------------------------------------------------\n");
 		System.out.println("Your HP: "+ playerHP);
 		System.out.println("Monster HP: " + monsterHP);
 		System.out.println("\n1: Attack");
 		System.out.println("2: Run");
 		System.out.println("\n------------------------------------------------------------------\n");
+		}
 		choice = myScanner.nextInt();
-		
+		Page chosenPage;
 		switch (choice) {
 			case 1:
-				attack();
+				chosenPage = Page.attack;
 				break;
 			case 2:
-				crossRoad();
+			chosenPage = Page.crossRoad;
 				break;
 			default:
-				fight();
+			chosenPage = Page.fight;
 		}
+		return chosenPage;
 	}
 	
-	private static void attack() {
+	private static Page attack() {
+		Page resultPage;
 		int playerDamage = 0;
-		
 		if (playerWeapon.equals("Knife")) {
 			playerDamage = myRandom.nextInt(5);
 		}
-		else if (playerWeapon.equals("Long Sword")) {
+		else {
 			playerDamage = myRandom.nextInt(8); 
 		}
-		System.out.println("You attacked the monster and gave " + playerDamage + " damage!");
 		monsterHP -= playerDamage;
+		System.out.println("You attacked the monster and gave " + playerDamage + " damage!");
 		System.out.println("Monster HP: " + monsterHP);
 		
-		if (monsterHP < 1) { winFight(); } else if (monsterHP > 0) {
-			int monsterDamage =0;
+		if (monsterHP < 1) { resultPage = Page.winFight; } 
+		else {
+			int monsterDamage = 0;
 			monsterDamage = myRandom.nextInt(4);
-			System.out.println("The monster attacked you and gave " + monsterDamage + " damage!");
 			playerHP -= monsterDamage;
+			if (playerHP < 1) { resultPage = Page.loseGame; } 
+			else { resultPage = Page.fight;}
+			System.out.println("The monster attacked you and gave " + monsterDamage + " damage!");
 			System.out.println("Player HP: " + playerHP);
-			if (playerHP < 1) { loseGame(); } else if (playerHP > 0) {
-				fight();
-			}
 		}
+		return resultPage;
 	}
 	
 	private static void loseGame() {
@@ -214,21 +277,23 @@ public class AdventureGame {
 		System.out.println("\n------------------------------------------------------------------\n");
 	}
 	
-	private static void winFight() {
+	private static Page winFight() {
+		silverRing = true;
+		{ //text
 		System.out.println("\n------------------------------------------------------------------\n");
 		System.out.println("You killed the monster!");
 		System.out.println("The monster dropped a ring!");
 		System.out.println("You obtaind a silver ring!\n\n");
 		System.out.println("1: Go east");
 		System.out.println("\n------------------------------------------------------------------\n");		
-		silverRing = true;
+		}
 		choice = myScanner.nextInt();
 		
 		if (choice == 1) {
-			crossRoad();
+			return Page.crossRoad;
 		}
 		else {
-			winFight();
+			return Page.winFight;
 		}
 	}
 	
